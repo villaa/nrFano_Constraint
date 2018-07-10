5089e75f759be0cbac94ab8c0b5c4d9f4fce37f5
Anthony Villano <anthony.villano@ucdenver.edu>
2018-07-10 13:14:01 -0500

    [ANV] updated the python resolution functions

--- python/resfuncRead.py
+++ python/resfuncRead.py
@@ -10,7 +10,7 @@ def getRFunc(infile):
   out = {}
 
   for i,line in enumerate(f.readlines()):
-    print(line)
+    #print(line)
     det = np.uint32(line.split()[0])
     out[det] = {}
     sqrt_vec = np.ones((6,),dtype=np.float64)
@@ -21,11 +21,20 @@ def getRFunc(infile):
     sqrt_vec[4] = np.float64(line.split()[5])
     sqrt_vec[5] = np.float64(line.split()[6])
     out[det]['sqrt'] = sqrt_vec 
+    lin_vec = np.ones((6,),dtype=np.float64)
+    lin_vec[0] = np.float64(line.split()[7])
+    lin_vec[1] = np.float64(line.split()[8])
+    lin_vec[2] = np.float64(line.split()[9])
+    lin_vec[3] = np.float64(line.split()[10])
+    out[det]['lin'] = lin_vec 
 
   return out
 
 def makeFunc(vec,islin = False):
 
-  f = lambda x: np.sqrt(vec[0] + x*vec[2] + x**2*vec[4]) 
+  if not islin:
+    f = lambda x: np.sqrt(vec[0] + x*vec[2] + x**2*vec[4]) 
+  else:
+    f = lambda x: vec[0] + x*vec[2]
 
   return f
