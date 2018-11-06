4562c8a675b253be6c8dd1414497cf4bc601ab62
Mathenym <Mitchell.Matheny@ucdenver.edu>
2018-11-05 23:44:10 -0700

    created functions

---
+++ Band_Fits/Nr_plot.py
@@ -0,0 +1,84 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import resfuncRead as rfr
+
+
+bpar_er = rfr.getBandFunc('data/erband_R133') #reads in band data/fit for er data
+bpar_nr = rfr.getBandFunc('data/nrband_R133') #reads in band data/fit for nr data 
+
+yer_mu = rfr.makeBFunc(bpar_er[1]['mu']) # sets average fit from 1st (2nd) col in data table. 
+yer_muv = np.vectorize(yer_mu) #puts mean data for er in 1D array 
+yer_sig = rfr.makeBFunc(bpar_er[1]['sig'],True) #sets uncertainty 
+yer_sigv = np.vectorize(yer_sig) #puts uncertainty into 1D array
+
+#following does the same but for nuclear recoils. 
+ynr_mu = rfr.makeBFunc(bpar_nr[1]['mu'])
+ynr_muv = np.vectorize(ynr_mu)
+ynr_sig = rfr.makeBFunc(bpar_nr[1]['sig'],True)
+ynr_sigv = np.vectorize(ynr_sig)
+
+
+def NR_band_plot(Enr,a):
+    X = Enr
+    X = np.sort(X)
+    fig,axes = plt.subplots(1,1,figsize=(9.0,8.0),sharex=True)
+    ax1 = axes
+
+    ax1.plot(Enr,a,'+',color='b',linewidth=2,markersize=3)
+    ax1.plot(X,ynr_mu(X)+1*ynr_sig(X),'r-',label = '1 $\sigma$')
+    ax1.plot(X,ynr_mu(X)-1*ynr_sig(X),'r-')
+
+    ax1.plot(X,ynr_mu(X)+2*ynr_sig(X),'m-',label = '2 $\sigma$')
+    ax1.plot(X,ynr_mu(X)-2*ynr_sig(X),'m-')
+
+    ax1.plot(X,ynr_mu(X)+3*ynr_sig(X),'k-',label = '3 $\sigma$')
+    ax1.plot(X,ynr_mu(X)-3*ynr_sig(X),'k-')
+
+    ax1.plot()
+    ax1.set_ylim(0,1)
+    ax1.set_xlim(0,200)
+    
+    
+    ax1.set_xlabel('$E_R$(keV)',size = '18')
+    ax1.set_ylabel('Yield',size = '18')
+    ax1.set_title('Data Check NR Band', size = '20')
+    ax1.tick_params(axis='both', labelsize = '20')
+    ax1.grid(True)
+    ax1.yaxis.grid(True,which='minor',linestyle='-')
+    
+
+    ax1.legend(loc=1,prop={'size':12})
+    plt.show()
+    
+    
+def ER_band_plot(Erer,b):
+    Y = Erer
+    Y = np.sort(Y)
+    fig,axes = plt.subplots(1,1,figsize=(9.0,8.0),sharex=True)
+    ax1 = axes
+
+    ax1.plot(Erer,b,'+',color='b',linewidth=2,markersize=3)
+    ax1.plot(Y,yer_mu(Y)+1*yer_sig(Y),'r-',label = '1 $\sigma$')
+    ax1.plot(Y,yer_mu(Y)-1*yer_sig(Y),'r-')
+
+    ax1.plot(Y,yer_mu(Y)+2*yer_sig(Y),'m-',label = '2 $\sigma$')
+    ax1.plot(Y,yer_mu(Y)-2*yer_sig(Y),'m-')
+
+    ax1.plot(Y,yer_mu(Y)+3*yer_sig(Y),'k-',label = '3 $\sigma$')
+    ax1.plot(Y,yer_mu(Y)-3*yer_sig(Y),'k-')
+
+    ax1.plot()
+    ax1.set_ylim(0,1.6)
+    ax1.set_xlim(0,200)
+    
+    
+    ax1.set_xlabel('$E_R$(keV)',size = '18')
+    ax1.set_ylabel('Yield',size = '18')
+    ax1.set_title('Data Check ER Band', size = '20')
+    ax1.tick_params(axis='both', labelsize = '20')
+    ax1.grid(True)
+    ax1.yaxis.grid(True,which='minor',linestyle='-')
+    
+
+    ax1.legend(loc=1,prop={'size':12})
+    plt.show()
---
+++ Band_Fits/Yield_calc.py
@@ -0,0 +1,169 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import resfuncRead as rfr
+import pandas as pd
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+from scipy.optimize import curve_fit
+
+ER = []
+Yield = []
+PtNr = []
+QR = []
+sigQ = []
+sigP = []
+sigQ1 = []
+sigP1 = []
+ENR = []
+U1 = []
+U2 = []
+N_e_h = []
+QNR=[]
+PT1 = []
+fano = []
+
+k = 0.157 # from lindhard
+q = 1.602*10**-19 #electron charge 
+V = 4.0 # voltage Bias
+eps = .0033 #keV
+
+#for detector 1
+p_alpha = 0.155393
+p_beta = 0 #9.60343*10**(-11)
+p_gamma = 0.000506287
+
+q_alpha = 0.166004
+q_beta = 0 #0.00233716
+q_gamma = 9.52576*10**(-5)
+
+def Yield_NR(N):
+    
+
+    import sys
+    sys.path.insert(0,'/Users/Mitch/Desktop/nrFano_Constraint/python')
+    import lindhard as lind
+    lpar = lind.getLindhardPars('Ge',True)
+
+    N = 10000
+    Er = np.random.uniform(0,200,N)
+    #Er = np.random.exponential(40,np.uint32(N*0.3))
+
+    for i in np.arange(0,N):
+
+        Enr = np.random.choice(Er)
+
+        a = 0.16
+        b = 0.18 
+        c = 0.04
+        A = Enr**(1-b)
+        B = Enr**(1-2*b)
+        C = Enr**(1-3*b)
+
+
+        #Yield Edelweiss 
+        #Y = a*Enr**b
+
+         #use the "calculated" value of k
+        Y = lind.getLindhard(lpar)
+
+
+        #EdelWeiss Fano
+
+        F = c**2/((eps*a/(A))+(2*V*a**2/(B*1000))+(2*(V/1000)**2*a**3/(eps*C)))
+
+
+        #Constant Fano
+        #F = 0
+
+        Neh = Y(Enr*1000)*Enr/eps #number of electron-hole pairs. 
+
+        N_eh = Neh + np.random.normal(0,np.sqrt(F*Neh))
+
+
+        Ptnr = Enr+(V/1000)*N_eh #central value of Pt
+        Qnr = N_eh*eps
+
+
+        #Without Fano Factor 
+        sig_p1 = np.sqrt(p_alpha + p_beta*Ptnr + p_gamma*(Ptnr**2) )  
+        sig_q1 = np.sqrt(q_alpha + q_beta*Qnr + q_gamma*(Qnr**2) )
+
+       #With Fano Factor. 
+        sig_p = np.sqrt(p_alpha + p_beta*Ptnr + p_gamma*(Ptnr**2))  #Phonon uncertainty (energy dependent)
+        sig_q = np.sqrt(q_alpha + q_beta*Qnr + q_gamma*(Qnr**2)) #Charge uncertainty 
+
+
+        Fnr = np.random.normal(0.0,sig_p) #random sample assuming phonon variance 
+        Fq = np.random.normal(0.0,sig_q) #random sampel assuming charge variance 
+
+
+        Ptnr1 = Ptnr + Fnr 
+
+        Qnr1 = Qnr + Fq 
+
+        Ernr = Ptnr1 - (V/(eps*1000))*Qnr1 #Meaured ER
+
+        yield1 = Qnr1 / Ernr #Measured Yield 
+
+
+
+        #Derivatives for Fano
+        U_1 = Ptnr/(Ptnr-Qnr1*(V/(eps*1000)))**2 #derivative with respect to Qnr1
+        U_2 = -Qnr1/(Ptnr1 -(Qnr1*V/(eps*1000)))**2      #derivative with respect to Ptnf
+
+
+
+        #Store Shit 
+        ENR.append(Enr)
+        N_e_h.append(Neh)
+        sigQ.append(sig_q)
+        sigP.append(sig_p)
+        sigQ1.append(sig_q1)
+        sigP1.append(sig_p1)
+        PT1.append(Ptnr1)
+        QNR.append(Qnr1)
+        ER.append(Ernr)
+        Yield.append(yield1)
+        U1.append(U_1)
+        U2.append(U_2)
+        fano.append(F)
+        
+    return ER, Yield 
+
+def Yield_er(N):
+    Yield_er = []
+    ERer = []
+    E1er = np.random.uniform(0,200,N) #from anthony, Er's are close enough to randomly distributed. 
+
+    QER = []
+
+    for i in np.arange(N):
+
+        Eer = np.random.choice(E1er) #randomly sample from Energy dist 
+
+        Pter = (1+(V/eps/1000))*Eer
+
+        N_e = Eer/eps 
+        N_er = np.random.normal(0,np.sqrt(0.1*N_e)) + N_e
+
+
+        sig_pee = np.sqrt(p_alpha + p_beta*Pter + p_gamma*(Pter**2)) #Phonon uncertainty 
+        sig_qee = np.sqrt(q_alpha + q_beta*Eer + q_gamma*(Eer**2)) #Charge uncertainty 
+
+        Fer = np.random.normal(0.0,sig_pee) #Random energy assuming phonon variance 
+        Fqe = np.random.normal(0.0,sig_qee) #Random assuming charge variane
+
+
+        Pter = Eer+(V/1000)*N_er
+        Pter = Pter + Fer
+        Qer = N_er*eps
+        Qer = Qer + Fqe
+        QER.append(Qer)
+
+        Erer = Pter - (V/eps/1000)*Qer
+        ERer.append(Erer)
+
+        Yield2 = Qer/Erer
+        Yield_er.append(Yield2)
+        
+    return ERer, Yield_er 
