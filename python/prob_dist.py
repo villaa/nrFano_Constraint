aca1c78a2195a0082364762e6ea693609f5bd0ab
Amy <Amy.L.Roberts@usd.edu>
2019-01-18 14:49:18 -0700

    finish prob_dist.py

--- python/prob_dist.py
+++ python/prob_dist.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # returns the probability of z, where z is the yield
 # z = Eq/(Ep - k*Eq)
@@ -9,7 +10,12 @@ import numpy as np
 # res_q = mu_q/sig_q
 # r = sig_p/sig_q
 # k is defined as e*voltage/(energy needed to create one e/h pair)
+# so for a Si detector at e.g. 4V, k ~ 4/3 
 def ratio_dist(z, res_p, res_q, r, k):
     F1 = np.exp(-0.5*(res_q**2 + res_p**2)) / (np.pi*(r*z**2 + (1/r)*(1+k*z)*2))
     
-    G11 = () / ()
+    G11 = (r*(z*res_q*r + (1+k*z)*res_p)) / (np.sqrt(2*np.pi)*np.power(z**2 * r**2 + (1+k*z)**2, 3/2))
+    G12 = np.exp(-(z*res_p*r - (1+k*z)*res_q)**2 / (2*(z**2 * r**2 + (1+k*z)**2)))
+    G13 = math.erf((z*res_q + (1+k*z)*res_p/r) / np.sqrt(2*(z**2 + (1+k*z)**2 / r**2)))
+
+    return F1 + G11*G12*G13
