bde13a76523a8a01692e32383106d2c30b42cc7a
Mathenym <Mitchell.Matheny@ucdenver.edu>
2019-04-24 09:50:40 -0600

    Made QQ plots

--- Mitch/bin_data_check.py
+++ Mitch/bin_data_check.py
@@ -7,7 +7,8 @@ from prob_dist import *
 from Dist_check import * 
 from scipy import integrate 
 from Hist_plot import * 
-
+import pylab 
+import scipy.stats as stats
 
     
         
@@ -55,7 +56,7 @@ def bin_check(df,s,band_func,bins,cut_idx,expected,Er_true,fano):
             
   
             bin_names.append(bin_name)
-  
+
 
             E = np.array(bin_data.E_measured)
             E_true = np.array(bin_data.E_true)
@@ -71,6 +72,9 @@ def bin_check(df,s,band_func,bins,cut_idx,expected,Er_true,fano):
              
 
             bincenters.append(np.mean(E_true))
+
+            bin_center = bin_name.mid
+            
             #print(x)
 
             #look at distributions graphically f
@@ -81,6 +85,13 @@ def bin_check(df,s,band_func,bins,cut_idx,expected,Er_true,fano):
             prob = dist_check(u,Ep_mean,Eq_mean,Sp_mean,Sq_mean,k) #amy's defined PDF 
             #prob = dist_check_fano(u,E,N_mean,Sp_mean,Sq_mean,SN)
             hist_plot(Yield,prob,u,bin_name,fano)  
+
+            plt.subplots(1,1,figsize=(9.0,8.0),sharex=True)
+            stats.probplot(Yield, dist="norm", plot=pylab)
+            pylab.title('Q-Q plot '+ str(bin_center)+' keV')
+            plt.grid(True)
+            plt.savefig('figures/Q-Q_plot '+str(bin_center)+' keV.png')
+            pylab.show()
             
         
             up,down,N = compare(Yield,upper_bound,lower_bound) # up and down are the number of data points OUTSIDE the bands. 
@@ -147,8 +158,7 @@ def bin_check(df,s,band_func,bins,cut_idx,expected,Er_true,fano):
     
     
     #bin = [10.7,25.2,40.3]
-    bin_center = [x.mid for x in bin_names]
-    print(bin_center)
+   
     
     
     #f = np.linspace(11,96,100)
