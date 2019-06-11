06bc7d12dd450d401ec159e492098d57891c2446
Anthony Villano <anthony.villano@ucdenver.edu>
2019-06-11 00:11:41 -0500

    [ANV] tweaked note to check out GGA1 parameters

--- python/prob_dist.py
+++ python/prob_dist.py
@@ -199,7 +199,8 @@ def sigroot(F,Er):
 #set the Edelweiss sigma definition to default to NR band
 def sigrootEdw(F,Er,V,eps,alpha=(1/100),Qbar=lambda x: 0.16*x**0.18):
 
-    fh2 = er.get_heatRes_func(0.4, 2.7)
+    #fh2 = er.get_heatRes_func(0.4, 2.7,0.035)
+    fh2 = er.get_heatRes_func(1.3, 2.7,0.035)
     heatRes_GGA3 = lambda x:(1/2.355)*fh2(x)
 
     fi2 = er.get_ionRes_func(1.3, 1.5, 3.1)
