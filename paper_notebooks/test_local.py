832d4fb7f17bd26e86b0aa0a2815d04826d1e8b2
Anthony Villano <anthony.villano@ucdenver.edu>
2019-07-05 13:23:30 -0500

    [ANV] updated travis build

---
+++ paper_notebooks/test_local.py
@@ -0,0 +1,21 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python3",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+    print('Testing Jupyter notebooks...')
+    #_exec_notebook('QEr_2D_joint.ipynb')
+    #_exec_notebook('ERNR_bands.ipynb')
+    #_exec_notebook('ms_correction.ipynb')
+    _exec_notebook('yield_width_compare.ipynb')
+    #_exec_notebook('EpEq_2D_joint.ipynb')
+    #_exec_notebook('EpEq_calc_contours.ipynb')
