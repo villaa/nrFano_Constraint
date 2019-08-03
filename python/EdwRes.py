[ANV] fixed a bug in the calculation of the NR Qwidth with Edw resolutions

python/EdwRes.py
@@ -188,9 +188,9 @@ def getEdw_det_res(label='GGA3',V=4.0,infile='data/edw_res_data.txt',aH=None,C=N
                                              *Qbar(Etr)**2*sigH_NR(Etr)**2)
     #add C if specified
     if C is not None:
-      sigQnr = lambda Etr: np.sqrt(sigQnr_base(Er)**2 + C**2)
+      sigQnr = lambda Etr: np.sqrt(sigQnr_base(Etr)**2 + C**2)
     else:
-      sigQnr = lambda Etr: np.sqrt(sigQnr_base(Er)**2)
+      sigQnr = lambda Etr: np.sqrt(sigQnr_base(Etr)**2)
 
     sigHv = np.vectorize(sigH)
     sigIv = np.vectorize(sigI)

python/fano_calc.py
@@ -38,6 +38,19 @@ def writeFano(file='fanoout.h5'):
   of.close()
   return
 
+def getFanoEdw(E=10,C=0.03):
+
+
+  #get the nominal width
+  lowsig = pd.sigmomEdw(E,alpha=(1/18.0))
+
+  findF = lambda F,Er,C: pd.sigmomEdw(Er,F=F,alpha=(1/18.0))**2 - lowsig**2 -C**2
+
+  Fout = so.brentq(findF,0.001,200,rtol=0.001,maxiter=100,args=(E,C,))
+  print('at energy E = {} keV NRFano is {}'.format(E,Fout))
+
+  return Fout
+
 def calcQWidth(n,F=10,V=4,eps=(3/1000),alpha=(1/100),Qbar=lambda x: 0.16*x**0.18,aH=0.035,path='./'):
 
   Er = np.linspace(7,100,n)
@@ -185,7 +198,7 @@ def storeQWidth(n,filename='test.h5',det='GGA3',band='ER',F=0.00001,V=4.0,alpha=
   print(den)
 
   #if density is comparable in given region
-  if (den>0.8)&(opt):
+  if (den>0.8)&(opt)&(~erase):
     cRemoveRange = (Er<emax_stored)&(Er>=emin_stored)
     Er = Er[~cRemoveRange]
 
@@ -207,3 +220,64 @@ def storeQWidth(n,filename='test.h5',det='GGA3',band='ER',F=0.00001,V=4.0,alpha=
   #print(sigcalc)
   (Er_new,sig_new) = RWCalc(filename,det,band,F,V,alpha,aH,Erv=E_needed,sigv=sigcalc,erase=erase)
   return (Er_new,sig_new)
+
+def RWCalcF(filename='test.h5',det='GGA3',band='ER',C=0.0346,V=4.0,alpha=(1/10000.0),aH=0.035,ErFv=None,Fv=None,erase=False):
+
+  #n=10
+  #Er = np.linspace(7,100,n)
+  #emin = np.min(Er)
+  #emax = np.max(Er)
+
+  #emins = '{:01.1f}'.format(emin)
+  #ns = '{:04.0f}'.format(n)
+  Fs = '{:03.0f}'.format(F)
+  Vs = '{:2.1f}'.format(V)
+  alphas = '{:1.3E}'.format(alpha)
+  aHs = '{:1.3f}'.format(aH)
+ 
+  path='{}/{}/{}/{}/{}/{}/'.format(det,band,Vs,alphas,aHs,Fs)
+
+  print(path)
+
+  #check for path
+  f = h5py.File(filename,'a')
+  exErF = path+'ErF' in f
+  exF = path+'F' in f
+  print(exErF)
+ 
+
+  #make some vector
+  if exErF&exF&~erase:
+    ErF = np.asarray(f[path+'ErF'])
+    F = np.asarray(f[path+'F'])
+  else:
+    ErF = np.zeros((0,))
+    F = np.zeros((0,))
+
+  #add in the data supplied
+  if (ErF is not None)&(Fv is not None):
+    ErF = np.append(ErF,ErFv)
+    F = np.append(F,Fv)
+
+  if exErF&exF:
+    del f[path+'ErF']
+    del f[path+'F']
+
+  #sort the array
+  idxErF = np.argsort(ErF)
+  ErF = ErF[idxErF]
+  F = F[idxErF]
+
+  ErF,uidx = np.unique(ErF,return_index=True)
+  F = F[uidx]
+
+  dset = f.create_dataset(path+'ErF',np.shape(ErF),dtype=np.dtype('float64').type, \
+  compression="gzip",compression_opts=9)
+  dset[...] = ErF
+  dset = f.create_dataset(path+'F',np.shape(ErF),dtype=np.dtype('float64').type, \
+  compression="gzip",compression_opts=9)
+  dset[...] = F 
+
+  f.close()
+
+  return (ErF,F)